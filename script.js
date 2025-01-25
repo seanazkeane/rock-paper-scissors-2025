0c4ffacf112994ddce9b5aa08707fb298371de13
Sean <seanazkeane@gmail.com>
2025-01-25 10:38:31 +0000

    Button selection logic and basic UI added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 // Declare human and computer scores
 let humanScore = 0;
 let computerScore = 0;
@@ -63,3 +64,77 @@ function playGame() {
     }
 }
 playGame();
+*/
+
+// Declare human and computer scores
+let humanScore = 0;
+let computerScore = 0;
+
+const display = document.querySelector('.display');
+
+// Get computer choice
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+// Create variables for buttons and start game everytime they're clicked
+
+function getHumanChoice() {
+    let answer = this.textContent.toLowerCase();
+    console.log(answer);
+    return answer;
+};
+
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => {
+    button.addEventListener('click', playRound)
+});
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = this.textContent.toLowerCase();
+    computerChoice = getComputerChoice();
+    if (humanChoice === 'rock') {
+        if (computerChoice ==='scissors') {
+            humanScore++;
+            display.textContent = ("Scissors beats paper! Human: " + humanScore + " - Computer " + computerScore);
+        } else if (computerChoice === 'paper') {
+            computerScore++;
+            display.textContent = ("Paper beats rock! Human: " + humanScore + " - Computer " + computerScore);
+        } else {
+            display.textContent = ("It's a draw! Human: " + humanScore + " - Computer " + computerScore);
+        }
+    } else if (humanChoice === 'paper') {
+        if (computerChoice ==='rock') {
+            humanScore++;
+            display.textContent = ("Paper beats rock! Human: " + humanScore + " - Computer " + computerScore);
+        } else if (computerChoice === 'scissors') {
+            computerScore++;
+            display.textContent = ("Scissors beats paper! Human: " + humanScore + " - Computer " + computerScore);
+        } else {
+            display.textContent = ("It's a draw! Human: " + humanScore + " - Computer " + computerScore);
+        }
+    } else {
+        if (computerChoice ==='rock') {
+            humanScore++;
+            display.textContent = ("Rock beats scissors! Human: " + humanScore + " - Computer " + computerScore);
+        } else if (computerChoice === 'paper') {
+            computerScore++;
+            display.textContent = ("Scissors beats paper! Human: " + humanScore + " - Computer " + computerScore);
+        } else {
+            display.textContent = ("It's a draw! Human: " + humanScore + " - Computer " + computerScore);
+        }
+    }
+    checkWinner();
+};
+
+function checkWinner() {
+    if (humanScore === 5) {
+        display.textContent = "Game over! Player wins. Play again?";
+        humanScore = 0;
+    } else if (computerScore === 5) {
+        display.textContent = "Game over! Computer wins. Play again?";
+        computerScore = 0;
+    };
+};
